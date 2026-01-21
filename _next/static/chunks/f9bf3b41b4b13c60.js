e61d2006f98b57e08af005a18d8d2bec409fdde8
Mathilda Holmström <anna.mathildah@gmail.com>
2026-01-21 14:55:06 +0100

    Updates

--- _next/static/chunks/f9bf3b41b4b13c60.js
+++ _next/static/chunks/f9bf3b41b4b13c60.js
@@ -3,6 +3,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/36433d5cc092ad3b.js",
   "static/chunks/0be0e45d3f20c1ba.js",
   "static/chunks/96fbae28c8dc7fb8.js",
-  "static/chunks/d554b223b0dc6ab5.css",
-  "static/chunks/turbopack-9e48e2fc90aa3783.js"
+  "static/chunks/8c2bb0e28938e516.css",
+  "static/chunks/turbopack-490731e352ad5ddf.js"
 ])
--- _next/static/dGhIVGqD6WjmwXjD7INbI/_buildManifest.js
+++ _next/static/dGhIVGqD6WjmwXjD7INbI/_buildManifest.js
@@ -1,9 +1,9 @@
 self.__BUILD_MANIFEST = {
   "/": [
-    "static/chunks/8243361b3a172890.js"
+    "static/chunks/877a1d95638bd68a.js"
   ],
   "/[slug]": [
-    "static/chunks/380225a940d36532.js"
+    "static/chunks/f1b3040dce962c25.js"
   ],
   "/_error": [
     "static/chunks/93f35fe9911726c0.js"
